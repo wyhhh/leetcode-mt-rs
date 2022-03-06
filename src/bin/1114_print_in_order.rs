922c9a8bc16c3c05da47936bd2bf4dbfbd70f2fd
暮歌丶 <329167139@qq.com>
2022-03-06 20:25:18 +0800

    add solution with semaphore on 1114

--- src/bin/1114_print_in_order.rs
+++ src/bin/1114_print_in_order.rs
@@ -5,9 +5,11 @@ use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::time::Duration;
+use std_semaphore::Semaphore;
 
 fn main() {
-    solution_with_monitor();
+    // solution_with_monitor();
+    solution_with_semaphore();
 }
 
 fn solution_with_monitor() {
@@ -38,6 +40,56 @@ fn solution_with_monitor() {
                 monitor.cv.notify_all();
 
                 format!("thread {no} res: {:?}", *g - 1)
+            })
+        },
+        Some(Duration::from_millis(2000)),
+    );
+}
+
+fn solution_with_semaphore() {
+    let mut data = [1, 2, 3];
+    let mut ss = SliceShuffler::new(&mut data);
+    // we test 10 times
+    let mut r: Round<10> = Round::new();
+
+    r.start(
+        || {
+            let s = Arc::new(Semaphore::new(0));
+            let s2 = Arc::new(Semaphore::new(0));
+            let ans = Arc::new(AtomicU32::new(1));
+
+            ss.run(move |no| {
+                match no {
+                    1 => {
+                        // when the no 1 thread coming, we directly print it.
+                        println!("print no {no}!");
+                        assert_eq!(no, ans.load(Ordering::SeqCst));
+                        ans.fetch_add(1, Ordering::SeqCst);
+
+                        // and then release the semaphore one
+                        s.release();
+                    }
+                    2 => {
+                        // at no 2, we just acquire blockly for the semaphore one ready(just release adding one).
+                        s.acquire();
+
+                        println!("print no {no}!");
+                        assert_eq!(no, ans.load(Ordering::SeqCst));
+                        ans.fetch_add(1, Ordering::SeqCst);
+
+                        // last we release the s2 for no 3 printing
+                        s2.release();
+                    }
+                    3 => {
+                        // same as the no 2
+                        s2.acquire();
+
+                        println!("print no {no}!");
+                        assert_eq!(no, ans.load(Ordering::SeqCst));
+                        ans.fetch_add(1, Ordering::SeqCst);
+                    }
+                    _ => unreachable!(),
+                }
             })
         },
         Some(Duration::from_millis(2000)),
