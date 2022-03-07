77400701d11b33dce350693f16b3987ad651fb7c
暮歌丶 <329167139@qq.com>
2022-03-07 22:26:36 +0800

    add 1195

---
+++ src/bin/1195_fizz_buzz_multithreaded.rs
@@ -0,0 +1,45 @@
+use std::sync::Barrier;
+
+// https://leetcode-cn.com/problems/fizz-buzz-multithreaded/
+fn main() {
+    solution_with_barrier();
+}
+
+fn solution_with_barrier() {
+    let b = &Barrier::new(4);
+
+    crossbeam_utils::thread::scope(|s| {
+        for no in 1..=4 {
+            s.spawn(move |_| {
+                for n in 1..=15 {
+                    match no {
+                        1 => {
+                            if n % 3 == 0 && n % 5 != 0 {
+                                println!("{n}: fizz");
+                            }
+                        }
+                        2 => {
+                            if n % 5 == 0 && n % 3 != 0 {
+                                println!("{n}: buzz");
+                            }
+                        }
+                        3 => {
+                            if n % 15 == 0 {
+                                println!("{n}: fizzbuzz");
+                            }
+                        }
+                        4 => {
+                            if n % 3 != 0 && n % 5 != 0 {
+                                println!("{n}: {n}");
+                            }
+                        }
+                        _ => unreachable!(),
+                    }
+                    // we just wait here
+                    b.wait();
+                }
+            });
+        }
+    })
+    .unwrap();
+}
